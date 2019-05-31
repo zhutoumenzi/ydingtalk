7b6a88a2b56a2cd351acf390afe62b9558e1af48
zhutoumenzi <577274441@qq.com>
2019-05-31 22:31:27 +0800

    客户文件

--- client.php
+++ client.php
@@ -5,3 +5,7 @@
  * Date: 2019/5/31
  * Time: 22:25
  */
+// 全局自动加载
+require __DIR__ . '/vendor/autoload.php';
+// 调用类方法，打印返回值字符串的长度
+var_dump(strlen(\xiaoyan\ydingtalk\Nail::requestByGet('http://www.baidu.com')));
