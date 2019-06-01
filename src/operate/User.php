1d28162d91e898542136eac6027c1e034682fa8e
zhutoumenzi <577274441@qq.com>
2019-06-01 17:44:28 +0800

    完成用户管理类

--- src/operate/User.php
+++ src/operate/User.php
@@ -128,4 +128,74 @@ class User extends Template
         $res = json_decode($data, true);
         return $res;
     }
+
+    /**
+     * 创建用户
+     * @param array $arr['userid'] //员工在当前企业内的唯一标识，也称staffId
+     * @param array $arr['name'] //成员名称。长度为1~64个字符
+     * @param array $arr['orderInDepts'] //在对应的部门中的排序,Map结构的json字符串, key是部门的Id, value是人员在这个部门的排序值
+     * @param array $arr['department'] //数组类型，数组里面值为整型，成员所属部门id列表
+     * @param array $arr['position'] //职位信息。长度为0~64个字符
+     * @param array $arr['mobile'] //手机号码，企业内必须唯一，不可重复。如果是国际号码，请使用+xx-xxxxxx的格式
+     * @param array $arr['tel'] //分机号，长度为0~50个字符，企业内必须唯一，不可重复
+     * @param array $arr['workPlace'] //办公地点，长度为0~50个字符
+     * @param array $arr['remark'] //备注，长度为0~1000个字符
+     * @param array $arr['email'] //邮箱。长度为0~64个字符。企业内必须唯一，不可重复
+     * @param array $arr['orgEmail'] //员工的企业邮箱，员工的企业邮箱已开通，才能增加此字段， 否则会报错
+     * @param array $arr['jobnumber'] //员工工号。对应显示到OA后台和客户端个人资料的工号栏目。长度为0~64个字符
+     * @param array $arr['isHide'] //是否号码隐藏,true表示隐藏,false表示不隐藏
+     * @param array $arr['isSenior'] //是否高管模式，true表示是，false表示不是。
+     * @param array $arr['extattr'] //扩展属性，可以设置多种属性
+     * @param array $arr['hiredDate'] //入职时间，Unix时间戳
+     * @return mixed
+     */
+    public function create($arr = [])
+    {
+        $url = self::$url.'/user/create?access_token='.self::$token;
+        $data = Ask::http($url, 'POST', $arr, self::$headers);
+        $res = json_decode($data, true);
+        return $res;
+    }
+
+    /**
+     * 更新用户
+     * @param array $arr['lang'] //通讯录语言(默认zh_CN另外支持en_US)
+     * @param array $arr['userid'] //员工在当前企业内的唯一标识，也称staffId
+     * @param array $arr['name'] //成员名称。长度为1~64个字符
+     * @param array $arr['department'] //数组类型，数组里面值为整型，成员所属部门id列表
+     * @param array $arr['orderInDepts'] //实际是Map的序列化字符串，Map的Key是deptId，表示部门id，Map的Value是order，表示排序的值，列表是按order的倒序排列输出的，即从大到小排列输出的
+     * @param array $arr['position'] //职位信息。长度为0~64个字符
+     * @param array $arr['mobile'] //手机号码，企业内必须唯一，不可重复。如果是国际号码，请使用+xx-xxxxxx的格式
+     * @param array $arr['tel'] //分机号，长度为0~50个字符，企业内必须唯一，不可重复
+     * @param array $arr['workPlace'] //办公地点，长度为0~50个字符
+     * @param array $arr['remark'] //备注，长度为0~1000个字符
+     * @param array $arr['email'] //邮箱。长度为0~64个字符。企业内必须唯一，不可重复
+     * @param array $arr['orgEmail'] //员工的企业邮箱，员工的企业邮箱已开通，才能增加此字段， 否则会报错
+     * @param array $arr['jobnumber'] //员工工号。对应显示到OA后台和客户端个人资料的工号栏目。长度为0~64个字符
+     * @param array $arr['isHide'] //是否号码隐藏,true表示隐藏,false表示不隐藏
+     * @param array $arr['isSenior'] //是否高管模式，true表示是，false表示不是。
+     * @param array $arr['extattr'] //扩展属性，可以设置多种属性
+     * @param array $arr['hiredDate'] //入职时间，Unix时间戳
+     * @return mixed
+     */
+    public function update($arr = [])
+    {
+        $url = self::$url.'/user/update?access_token='.self::$token;
+        $data = Ask::http($url, 'POST', $arr, self::$headers);
+        $res = json_decode($data, true);
+        return $res;
+    }
+
+    /**
+     * 删除用户
+     * @param $userid
+     * @return mixed
+     */
+    public function delete($userid)
+    {
+        $url = self::$url.'/user/delete?access_token='.self::$token.'&userid='.$userid;
+        $data = Ask::http($url, 'GET', null, self::$headers);
+        $res = json_decode($data, true);
+        return $res;
+    }
 }
